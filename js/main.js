2cd6777681e059a481bb1e45c216f9c9d69e1a64
Antony Ndambuki <143907986+Anto-4ny@users.noreply.github.com>
2025-11-05 12:08:52 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,29 @@
 (function ($) {
     "use strict";
 
-    // Navbar on scrolling
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.navbar').fadeIn('slow').css('display', 'flex');
-        } else {
-            $('.navbar').fadeOut('slow').css('display', 'none');
-        }
-    });
+    // Navbar show/hide on scroll
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+        $('.navbar').fadeIn('slow').css('display', 'flex');
+    } else {
+        $('.navbar').fadeOut('slow').css('display', 'none');
+    }
+});
 
+// Smooth scrolling for links
+$(".navbar-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
 
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 45
+        }, 1500, 'easeInOutExpo');
 
+        // Active state update
+        $('.navbar-nav .active').removeClass('active');
+        $(this).addClass('active');
+    }
+});
 
     // Typed Initiate
     if ($('.typed-text-output').length == 1) {
@@ -114,4 +111,5 @@
     });
     
 })(jQuery);
+
 
